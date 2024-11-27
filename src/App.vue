7bca7122d4dbb2034c71caa72d33274ceb99f75b
MaitreGobz <nayetlucas@hotmail.fr>
2024-11-27 18:34:31 +0100

    footer done

--- src/App.vue
+++ src/App.vue
@@ -7,26 +7,43 @@ import { RouterLink, RouterView } from 'vue-router'
   <!-- Permanent header navbar -->
 
   <header>
-    <div class="menuNav">
-      <nav>
-        <menu>
-          <li type="none" id="liHome"><router-link to="/" >Acceuil</router-link></li>
-          <li type="none" id="liPres"><a href="#presentationDiv">Présentation</a></li>
-          <li type="none" id="liProject"><a href="#projectDiv">Mes projets</a></li>
-        </menu>
-      </nav>     
-    </div>
-    <div class="logo">
-      <router-link to="/" >
-        <img alt="Lucas Nayet's logo" id="logoLN" src="/src/assets/Images/Logo 2.png" width="50" height="66" />
-      </router-link> 
-    </div>
-    <div class="contactLink">
-      <a href="#contactDiv" class="contactLink">Contact</a>
+    <div class="header">
+      <div class="menuNav">
+        <nav>
+          <menu>
+            <li type="none" id="liHome"><router-link to="/" >Acceuil</router-link></li>
+            <li type="none" id="liPres"><a href="#presentationDiv">Présentation</a></li>
+            <li type="none" id="liProject"><a href="#projectDiv">Mes projets</a></li>
+          </menu>
+        </nav>     
+      </div>
+      <div class="logo">
+        <router-link to="/" >
+          <img alt="Lucas Nayet's logo" id="logoLN" src="/src/assets/Images/Logo 2.png" width="50" height="66" />
+        </router-link> 
+      </div>
+      <div class="contactLink">
+        <a href="#contactDiv" class="contactLink">Contact</a>
+      </div>
     </div>
   </header>
-
-
+  <footer>
+    <div id="footer">
+      <div class="rsLink">
+        <a href="https://github.com/MaitreGobz" target="_blank">
+          <img src="./assets/images/github.png" class="rsLink" alt="Lien vers profil GitHub">
+        </a>
+      </div>
+      <div class="rsLink">
+        <a href=" https://x.com/maitregobz" target="_blank">
+          <img src="./assets/images/twitter.png" class="rsLink" alt="Lien vers profil X">
+        </a>
+      </div>
+    </div>
+    <div id="small">
+        <p><small>Dernière mise à jour le 17/09/2024</small></p>
+    </div>
+  </footer>
 
   <RouterView />
 </template>
@@ -35,7 +52,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 /* NavBar's and menu's style */
 
-header {
+.header {
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -71,10 +88,6 @@ div.logo {
   margin-right: 350px;
 }
 
-#logoLN {
-  position: sticky;
-}
-
 nav a.router-link-exact-active {
   text-decoration: underline;
 }
@@ -82,6 +95,31 @@ nav a.router-link-exact-active {
 nav a:hover,
 a.contactLink:hover {
   font-size: 1.4rem;
+}
+#footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 20px;
+    margin-bottom: 5px;
+}
+
+img.rsLink {
+    width: 50px;
+    height: 50px;
+    margin-right: 5px;
+}
+
+#small {
+  text-align: center;
+  font-size: 0.7rem;
+}
+
+img.rsLink:hover {
+  width: 60px;
+  height: 60px;
+  transition: 0.8s;
+  opacity: 0.50;
 }
 
 </style>
