13062753768e5324453393ecd772a04ca2647dd7
MaitreGobz <nayetlucas@hotmail.fr>
2024-11-30 17:54:57 +0100

    errorPage done

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="error">
-    <h1>Erreur 404</h1>
+    <picture class="imageError">
+      <img src="/src/assets/Images/Error404(1).png" alt="image erreur">
+    </picture>
+  </div>
+  <div class="error404">
+      <h1>404</h1>
+      <router-link to="/" class="retrunHome"><a href="#" class="returnHome">Accueil</a></router-link>
   </div>
 </template>
 
-<style>
+<style scoped>
+
+div.error404 {
+  text-align: center;
+  color: var(--wht);
+  font-size:2.2rem;
+}
+
+picture {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.imageError img {
+  width: 35% ;
+  height: 35%;
+}
+
+a.returnHome:hover {
+  font-size: 2.5rem;
+  text-decoration: underline;
+}
 
 </style>
